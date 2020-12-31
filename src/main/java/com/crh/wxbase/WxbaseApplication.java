aa40b351f5f50a697afb93b3fe2685e6c5525eb9
chenrenhao <rory.chen@jfz.com>
2020-12-31 18:53:32 +0800

    init

---
+++ src/main/java/com/crh/wxbase/WxbaseApplication.java
@@ -0,0 +1,13 @@
+package com.crh.wxbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxbaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxbaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crh/wxbase/WxbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crh.wxbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxbaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
